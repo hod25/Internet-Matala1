b7ff98a29258c33605f7a6bf5b9f0c495c164ec5
hod25 <hod7112000@gmail.com>
2024-12-07 12:56:00 +0200

    models

---
+++ src/models/Comment.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IComment extends Document {
+  content: string;
+  post: mongoose.Types.ObjectId;
+  user: mongoose.Types.ObjectId;
+}
+
+const CommentSchema = new Schema<IComment>({
+  content: { type: String, required: true },
+  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+export default mongoose.model<IComment>("Comment", CommentSchema);
